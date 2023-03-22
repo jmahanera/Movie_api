3d2690b30bec05b5cb81515cbea74a0927c2b283
jmahanera <122871245+jmahanera@users.noreply.github.com>
2023-03-22 12:48:18 +0100

    App pages added to repository

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+
+  if (pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (pathname === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the Movie Home page.');
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found');
+    res.end();
+  }
+});
+
+server.listen(8080, () => {
+  console.log('My first Node test server is running on Port 8080.');
+});
