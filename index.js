acb7275d1cf68f0b237eaef8778a187d34755663
jula <122871245+jmahanera@users.noreply.github.com>
2023-03-30 10:32:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const express = require('express');
 const app = express();
 
 
-const http = require('http'),
-  url = require('url');
+const http = require('http');
 
 http.createServer((request, response) => {
-  let requestURL = url.parse(request.url, true);
-  if ( requestURL.pathname == '/documentation.html') {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Documentation on the bookclub API.\n');
-  } else {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Welcome to my book club!\n');
-  }
-
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Welcome to my book club!\n');
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
