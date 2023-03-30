e215e1becf2487a11e41b10b9126f8ddf12e2038
jula <122871245+jmahanera@users.noreply.github.com>
2023-03-30 11:48:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const path = require('path');
 // create an instance of express
 const app = express();
 
- // serve static files from the "public" folder
- app.use(express.static(path.join(__dirname, 'public')));
+
 
 // define a route that returns a JSON object containing data about your top 10 movies
 app.get('/movies', (req, res) => {
@@ -70,6 +69,9 @@ app.get('/movies', (req, res) => {
 app.get('/', (req, res) => {
     res.send('These are my Favorite Movies!');
   });
+
+   // serve static files from the "public" folder
+ app.use(express.static(path.join(__dirname, 'public')));
 
  
 
