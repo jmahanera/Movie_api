3d145220d6012b3d08b06642878045725f98ae5e
jula <122871245+jmahanera@users.noreply.github.com>
2023-03-30 11:20:32 +0200

    Express required

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-//const express = require('express');//
-//const app = express();//
-
-
-const express = require('express'),
-  morgan = require('morgan');
-
+const express = require('express');
 const app = express();
 
-app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
-});
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
-});
 
 
 
