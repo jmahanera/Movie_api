28ac2514847ee741896ef276b8814d6d74d70748
jula <122871245+jmahanera@users.noreply.github.com>
2023-05-24 00:38:42 +0200

    Update models.js

--- models.js
+++ models.js
@@ -18,11 +18,10 @@ const movieSchema = mongoose.Schema({
   });
   
   const userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthday: Date,
   });
   
   const Movie = mongoose.model('Movie', movieSchema);
