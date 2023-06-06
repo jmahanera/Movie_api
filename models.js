66d03ec307a0b847553655dbe81cd5bb51910a99
jula <122871245+jmahanera@users.noreply.github.com>
2023-06-06 12:55:53 +0200

    added bcrypt and updated userschema code

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -15,17 +16,25 @@ const movieSchema = mongoose.Schema({
     Actors: [String],
     ImagePath: String,
     Featured: Boolean
-  });
-  
-  const userSchema = mongoose.Schema({
+});
+
+const userSchema = mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     birthday: Date,
-  });
-  
-  const Movie = mongoose.model('Movie', movieSchema);
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+});
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
