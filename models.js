24edcdd6b2924f3fe99266f766f9b7164f9abe54
jula <122871245+jmahanera@users.noreply.github.com>
2023-07-05 11:18:08 +0200

    Update models.js

--- models.js
+++ models.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
-    birthday: Date,
+    birthdate: Date,
 });
 
 userSchema.statics.hashPassword = (password) => {
