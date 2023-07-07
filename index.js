f35ad097e8645f804870d9365ebae4afe6f69974
jula <122871245+jmahanera@users.noreply.github.com>
2023-07-07 10:13:16 +0200

    server port change

--- index.js
+++ index.js
@@ -304,7 +304,7 @@ app.put('/users/:username', passport.authenticate('jwt', { session: false }), (r
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
