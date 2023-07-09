80d928ffcb6fd9097bfa7df7378b85843ba26261
jula <122871245+jmahanera@users.noreply.github.com>
2023-07-09 17:00:33 +0200

    Update models.js

--- models.js
+++ models.js
@@ -2,37 +2,46 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const movieSchema = mongoose.Schema({
-    Title: { type: String, required: true },
-    Description: { type: String, required: true },
-    Genre: {
-        Name: String,
-        Description: String
-    },
-    Director: {
-        Name: String,
-        Biography: String,
-        Birthyear: String,
-    },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Biography: String,
+    Birthyear: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
 });
 
-const Movie = mongoose.model('Movie', movieSchema); // Initialize Movie variable here
+const Movie = mongoose.model('Movie', movieSchema);
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    birthDate: Date,
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthDate: Date,
 });
 
-userSchema.statics.hashPassword = function (password) {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(password, salt);
+UserSchema.methods.validatePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, result) {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, result);
+    });
+  };
+  
+  const User = mongoose.model('User', UserSchema);
+  
+  module.exports = {
+    User: User
   };
 
-const User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
