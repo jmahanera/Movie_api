4c3e42d42cf5744e5c2d5aae11baab8ed08e6264
jula <122871245+jmahanera@users.noreply.github.com>
2023-08-29 19:32:02 +0200

    Update main-view.jsx

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react"; // Import React and useState
 import { MovieCard } from "movie-card/movie-card";
 import { MovieView } from "movie-view/movie-view";
 
@@ -9,25 +9,24 @@ export const MainView = () => {
       description: "A theme park with cloned dinosaurs goes wrong",
       director: "Steven Spielberg",
       imageurl: "https://www.imdb.com/title/tt0107290/?ref_=nv_sr_srsg_0_tt_8_nm_0_q_Jurassic%2520Park"
-      },
-
-     { id: "644a352ca33d5b41dd203afb",
-       title: "Matrix",
-       description: "A Hacker discovers he is living in a simulated reality",
-       director: "Lana Wachowski",
-       imageurl: "https://www.imdb.com/title/tt0133093/",
-      },
-     { id: "644a35e0a33d5b41dd203afc",
-       title: "Goodfellas",
-       description: "A young man rises through the ranks of the Mafia",
-       director: "Martin Scorsese",
-       imageurl: "https://www.imdb.com/title/tt0099685/?ref_=fn_al_tt_1",
-      },
+    },
+    { id: "644a352ca33d5b41dd203afb",
+      title: "Matrix",
+      description: "A Hacker discovers he is living in a simulated reality",
+      director: "Lana Wachowski",
+      imageurl: "https://www.imdb.com/title/tt0133093/",
+    },
+    { id: "644a35e0a33d5b41dd203afc",
+      title: "Goodfellas",
+      description: "A young man rises through the ranks of the Mafia",
+      director: "Martin Scorsese",
+      imageurl: "https://www.imdb.com/title/tt0099685/?ref_=fn_al_tt_1",
+    },
   ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-    if (selectedMovie) {
+  if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
@@ -36,9 +35,11 @@ export const MainView = () => {
   if (movies.length === 0) {
     return <div> The list is Empty!</div>;
   }
+
   return (
     <div>
-      {movie.map((movie) => (
+      {/* Map through the movies array */}
+      {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           movie={movie}
