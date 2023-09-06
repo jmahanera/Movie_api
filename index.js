94be3235c4aa8e30566b9fcefb89eb47ed87c190
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-06 14:31:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -167,6 +167,7 @@ app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req
     });
 });
 
+//Get a movie using genreName
 app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), (req, res) => {
   const genreName = req.params.genreName;
 
@@ -200,6 +201,7 @@ app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: fals
     });
 });
 
+//Get all movies with the sane genre
 app.get('/movies/genre/:genreName/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   const genreName = req.params.genreName;
   // Log the genreName you are searching for
@@ -222,23 +224,6 @@ app.get('/movies/genre/:genreName/movies', passport.authenticate('jwt', { sessio
       res.status(500).json({ error: 'Internal server error' });
     });
 });
-
-
-/*app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.find({ 'genre.name': req.params.genreName })
-    .then((movies) => {
-      if (movies.length === 0) {
-        return res.status(404).send('Error: no movies found with this genre ' + req.params.genreName + ' genre type.');
-      } else {
-        return res.status(200).json(movies);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});*/
-
 
 
  app.get('/movies/directors/:directorName', passport.authenticate('jwt', { session: false }),
