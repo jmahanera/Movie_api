aa081f47f8eb40e5eeebcfaff44958aecca6d595
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-07 09:20:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const Users = Models.User;
 //
 
 // Connect to MongoDB
-const uri = 'mongodb+srv://jula:Davinci@12@moviecluster.1wliibn.mongodb.net/mymoviesDB?retryWrites=true&w=majority'; // Replaced with my MongoDB connecmongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = 'mongodb+srv://jula:Davinci@12@cluster.1wliibn.mongodb.net/mymoviesDB?retryWrites=true&w=majority'; // Replaced with my MongoDB connecmongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
 //mongoose.connect( "mongodb://127.0.0.1:27017/mymoviesDB",{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
