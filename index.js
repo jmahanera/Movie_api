0d566223b9fb5a49440c3d48758ea6a102b999a4
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-10 12:40:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const Users = Models.User;
 //
 
 // Connect to MongoDB
-/*const uri = 'mongodb+srv://julayamu:Myproject23@moviecluster.uviwjhs.mongodb.net/mymoviesDB?retryWrites=true&w=majority'; // Replaced with my MongoDB connecmongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = 'mongodb+srv://julayamu:Myproject23@moviecluster.uviwjhs.mongodb.net/mymoviesDB?retryWrites=true&w=majority'; // Replaced with my MongoDB connecmongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true })
 //mongoose.connect( "mongodb://127.0.0.1:27017/mymoviesDB",{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -33,13 +33,13 @@ mongoose.connect(process.env.MOVIES_URI || uri, { useNewUrlParser: true, useUnif
   })
   .catch((error) => {
     console.error('Error connecting to database:', error);
-  });*/
+  });
 
   
   /*mongoose.connect( process.env.movies_uri, { useNewUrlParser: true, useUnifiedTopology: true });*/
 
 
-const uri = process.env.MOVIES_URI || 'mongodb+srv://julayamu:Myproject23@moviecluster.uviwjhs.mongodb.net/mymoviesDB?retryWrites=true&w=majority';
+/*const uri = process.env.MOVIES_URI || 'mongodb+srv://julayamu:Myproject23@moviecluster.uviwjhs.mongodb.net/mymoviesDB?retryWrites=true&w=majority';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -47,7 +47,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch((error) => {
     console.error('Error connecting to database:', error);
-  });
+  });*/
 
 
 
@@ -371,6 +371,54 @@ app.post('/movies', passport.authenticate('jwt', { session: false }), async (req
     console.error(error);
     res.status(500).send('Error: ' + error);
   }
+});
+
+// Create a new director and associated movies
+app.post('/directors/directorName', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Extract director data from the request body
+  const { name, birthYear, bio, movies } = req.body;
+
+  // Create an array to store the movie IDs associated with the director
+  const movieIds = [];
+
+  // Loop through the list of movie titles and find or create them in the database
+  Promise.all(
+    movies.map(async (movieTitle) => {
+      const existingMovie = await Movies.findOne({ title: movieTitle });
+
+      if (existingMovie) {
+        movieIds.push(existingMovie._id);
+      } else {
+        const newMovie = new Movies({ title: movieTitle });
+        const savedMovie = await newMovie.save();
+        movieIds.push(savedMovie._id);
+      }
+    })
+  )
+    .then(() => {
+      // Create a new director object with the associated movie IDs
+      const newDirector = new Directors({
+        name,
+        birthYear,
+        bio,
+        movies: movieIds,
+      });
+
+      // Save the director to the database
+      return newDirector.save();
+    })
+    .then((director) => {
+      // Log the newly created director to the console
+      console.log('New Director Created:');
+      console.log(JSON.stringify(director, null, 2)); // Pretty-print JSON
+
+      // Send a response to the client
+      res.status(201).json(director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 
