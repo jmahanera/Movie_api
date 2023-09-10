d7e8fecb3ca89f0d6a72507ff1eed3e5653668b8
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-10 13:34:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -605,7 +605,6 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const port = process.env.PORT || 8080   
-
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
