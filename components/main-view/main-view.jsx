4fbe3a6034509a93cac33d3315d58c9a9eece8a2
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-14 09:57:51 +0200

    Movie urls updated

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -8,19 +8,19 @@ export const MainView = () => {
       title: "Jurassic Park",
       description: "A group of people visit a remote island where cloned dinosaurs have been unleashed, leading to a fight for survival.",
       director: "James Cameron",
-      imageurl: "https://www.imdb.com/title/tt0163025/?ref_=fn_al_tt_2"
+      imageurl: "https://eskipaper.com/images/jurassic-park-wallpaper-3.jpg"
     },
     { id: "64fc9aaf191da30c43fb6965",
       title: "Titanic",
       description: "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
       director: "The Wachowskis",
-      imageurl: "https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304/?ref_=tt_ov_i",
+      imageurl: "https://eskipaper.com/images/titanic-movie-1.jpg",
     },
     { id: "64fc9b13191da30c43fb6968",
       title: "The Matrix",
       description: "A computer programmer discovers that reality as he knows it is a simulation created by machines to subjugate humanity.",
       director: "Robert Zemeckis",
-      imageurl: "https://www.imdb.com/title/tt0133093/mediaviewer/rm525547776/?ref_=tt_ov_i",
+      imageurl: "https://eskipaper.com/images/neo-matrix-1.jpg",
     },
   ]);
 
