6b1e46fac32a09c5e7979021cdfeb60ceba1a55c
jula <122871245+jmahanera@users.noreply.github.com>
2023-09-24 18:21:57 +0200

    updated

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const hashPassword = async (password) => {
 
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234','http://localhost:8080','https://primemovies-39075872fbeb.herokuapp.com/api/movies'];
+let allowedOrigins = ['http://localhost:1234','http://localhost:8080','https://primemovies-39075872fbeb.herokuapp.com/movies'];
 
 app.use(cors({
   origin: (origin, callback) => {
